f7e444b1156b03ef31187f2c1c00ed98e57a0ff5
Vunb <nhubaovu@gmail.com>
2019-07-30 11:13:33 +0700

    init template project

---
+++ README.md
@@ -0,0 +1,10 @@
+## Ocelot: Sample Gateway Implementation
+
+This is a sample implementation of a gateway using Ocelot, .net Core 2.2 and docker.
+
+To find out more about Ocelot, you can check their official [documentation](http://ocelot.readthedocs.io) and their [repository](https://github.com/vunb/Ocelot)
+
+### Pre-requisites
+
+* Microsoft Visual Studio Community 2017 15.9+
+* Docker
---
+++ ServiceA/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceA.Controllers
+{
+    [Route("values")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<ServiceAModel> Get()
+        {
+            return new []
+            {
+                new ServiceAModel{Name = "Google",Url = "https://www.google.com.au"},
+                new ServiceAModel{Name = "SSW",Url = "https://www.ssw.com.au"},
+                new ServiceAModel{Name = "eBay",Url = "https://www.ebay.com.au/"},
+            };
+        }
+    }
+
+    public class ServiceAModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
